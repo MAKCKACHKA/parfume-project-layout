e184b8584d79518630b0a66ef118a4744458c240
MAKCKACHKA <doubleakop@gmail.com>
2023-11-23 23:36:34 +0200

    2

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,11 @@
 const glide = new Glide('.glide', {
   type: 'carousel',
   focusAt: 'center',
-    perView: 2,
-  breakpoints:{768: {
-    perView: 2
-  }}
+  perView: 1,
+  breakpoints: {
+    768: {
+      perView: 1,
+    },
+  },
 });
 glide.mount();
